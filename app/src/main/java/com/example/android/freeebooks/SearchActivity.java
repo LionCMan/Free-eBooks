dd5e171e8efd74b3ad36233084bcb6c0cb31e2db
LionCMan <cian.nolan@yahoo.co.uk>
2017-05-30 16:47:11 +0100

    RecyclerView & LayoutManager setup

--- app/src/main/java/com/example/android/freeebooks/SearchActivity.java
+++ app/src/main/java/com/example/android/freeebooks/SearchActivity.java
@@ -1,16 +1,52 @@
 package com.example.android.freeebooks;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
 
     private static final String BOOK_REQUEST_URL =
             "https://www.googleapis.com/books/v1/volumes?q=";
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView = (RecyclerView) findViewById(R.id.list_view);
+        mRecyclerView.setVisibility(View.GONE);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        TextView state = (TextView) findViewById(R.id.state_text);
+        state.setText(R.string.empty_view);
+        state.setVisibility(View.VISIBLE);
+
+        final EditText searchText = (EditText) findViewById(R.id.search_text);
+
+        ProgressBar loading = (ProgressBar) findViewById(R.id.loading);
+        loading.setVisibility(View.GONE);
+
+        ImageView searchButton = (ImageView) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Check Network Connection
+                // TODO: If Connected {...} else {...}
+            }
+        });
+
+
     }
 }
